32a75e32869869c36ad8bcc2e4f3a5f00d8db58f
philapinyo <c0044807@myhallam,shu.ac.uk>
2022-12-07 04:03:37 +0000

    inputs

--- adduser.sh
+++ adduser.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
+echo "Welcome"
 echo "Do you want to add a new user? "
 
 read -n1 -p "[y,n]" doit 
 case $doit in  
   y|Y) add_user ;; 
-  n|N) echo no ;; 
-  *) echo dont know ;; 
+  n|N) exit ;; 
+  *) exit echo "Invalid input";; 
 esac
 
 add_user()
 {
+    echo "Please enter a user name: "
+    read username
 
-    user=$1
-    password=$2
-    shift; shift;
-    comments=$@
+    echo "Please enter a password: "
+    read password
+    
     echo "Adding the user $user.."
-    echo useradd -c "$comments" $user
+    echo useradd -c $username
     echo passwd --stdin $user $password
     echo "Added user $user with password $password"
     
 }
-
-echo "script started"
-add_user
 
 
 
